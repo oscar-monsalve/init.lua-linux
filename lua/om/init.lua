ba59e048cabc7ac55503506262caea3b9c626e3a
oscar-monsalve <oscarmonsalvec5@gmail.com>
2024-01-27 19:33:07 -0500

    added yank highlighting

--- lua/om/init.lua
+++ lua/om/init.lua
@@ -1,2 +1,29 @@
 require("om.remap")
 require("om.set")
+
+local augroup = vim.api.nvim_create_augroup
+local omGroup = augroup('omGroup', {})
+
+local autocmd = vim.api.nvim_create_autocmd
+local yank_group = augroup('HighlightYank', {})
+
+function R(name)
+    require("plenary.reload").reload_module(name)
+end
+
+autocmd('TextYankPost', {
+    group = yank_group,
+    pattern = '*',
+    callback = function()
+        vim.highlight.on_yank({
+            higroup = 'IncSearch',
+            timeout = 40,
+        })
+    end,
+})
+
+autocmd({"BufWritePre"}, {
+    group = omGroup,
+    pattern = "*",
+    command = [[%s/\s\+$//e]],
+})
