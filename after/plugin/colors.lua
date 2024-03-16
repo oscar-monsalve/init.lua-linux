c686e73a0079901398edb6ddf8a0b95092eea31f
oscar-monsalve <oscarmonsalvec5@gmail.com>
2024-03-15 23:35:14 -0500

    update

--- after/plugin/colors.lua
+++ after/plugin/colors.lua
@@ -1,25 +1,30 @@
-require("tokyonight").setup({
-	style = "night", -- The theme comes in three styles, `storm`, `moon`, a darker variant `night` and `day`
-	transparent = true, -- Enable this to disable setting the background color
-	terminal_colors = true, -- Configure the colors used when opening a `:terminal` in Neovim
-	styles = {
-		-- Style to be applied to different syntax groups
-		-- Value is any valid attr-list value for `:help nvim_set_hl`
-		comments = { italic = false },
-		keywords = { italic = false },
-		-- Background styles. Can be "dark", "transparent" or "normal"
-		sidebars = "dark", -- style for sidebars, see below
-		floats = "dark", -- style for floating windows
-	},
-})
+function Colors(color)
+    color = color or "rose-pine"
+    vim.cmd.colorscheme(color)
 
-function ColorMyPencils(color)
-	color = color or "tokyonight"
-	vim.cmd.colorscheme(color)
-
-	vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
-	vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+    vim.api.nvim_set_hl(0, "Normal", { bg = "black" })
+    vim.api.nvim_set_hl(0, "NormalFloat", { bg = "black" })
 
 end
 
-ColorMyPencils()
+Colors()
+
+
+require("tokyonight").setup({
+    style = "night", -- The theme comes in three styles, `storm`, `moon`, a darker variant `night` and `day`
+    transparent = true, -- Enable this to disable setting the background color
+    terminal_colors = true, -- Configure the colors used when opening a `:terminal` in Neovim
+    styles = {
+        -- Style to be applied to different syntax groups
+        -- Value is any valid attr-list value for `:help nvim_set_hl`
+        comments = { italic = false },
+        keywords = { italic = false },
+        -- Background styles. Can be "dark", "transparent" or "normal"
+        sidebars = "dark", -- style for sidebars, see below
+       floats = "dark", -- style for floating windows
+    },
+})
+
+require('rose-pine').setup({
+    disable_background = true
+})
