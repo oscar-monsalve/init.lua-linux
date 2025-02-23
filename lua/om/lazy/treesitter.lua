68942f81aec2da40fdb01366f38350067d83200d
oscar monsalve <oscarmonsalvec5@gmail.com>
2025-02-22 19:28:11 -0500

    update

--- lua/om/lazy/treesitter.lua
+++ lua/om/lazy/treesitter.lua
@@ -4,7 +4,7 @@ return {
     config = function()
         require("nvim-treesitter.configs").setup({
             -- A list of parser names, or "all"
-            ensure_installed = {"c", "python", "todotxt", "markdown", "markdown_inline"},
+            ensure_installed = {"c", "zig", "python", "lua", "markdown", "markdown_inline"},
 
             -- Install parsers synchronously (only applied to `ensure_installed`)
             sync_install = false,
--- lua/om/remap.lua
+++ lua/om/remap.lua
@@ -23,6 +23,7 @@ vim.keymap.set("x", "<leader>p", [["_dP]])
 vim.keymap.set({"n", "v"}, "<leader>y", [["+y]])
 vim.keymap.set("n", "<leader>Y", [["+Y]])
 
+-- next greatest remap ever : asbjornHaland
 -- This is going to get me cancelled
 vim.keymap.set("i", "<C-c>", "<Esc>")
 
@@ -32,3 +33,5 @@ vim.keymap.set("n", "Q", "<nop>")
 vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
 
 vim.keymap.set("n", "<leader>x", "<cmd>!chmod +x %<CR>", { silent = true })
+
+
