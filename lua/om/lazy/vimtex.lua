2265bbac1f77d135d67b990d7ad91aa25ab0799f
Oscar Monsalve <oscarmonsalvec5@gmail.com>
2025-08-08 23:05:01 -0500

    updated vimtex.lua

--- lua/om/lazy/vimtex.lua
+++ lua/om/lazy/vimtex.lua
@@ -3,11 +3,15 @@ vim.g.maplocalleader = ","
 
 return {
     "lervag/vimtex",
+    -- lazy = false,  -- we don't want to lazy load VimTex
 
     init = function()
         -- PDF viewer
-        vim.g["vimtex_view_general_viewer"] = "okular"
-        vim.g["vimtex_view_general_options"] = "--unique file:@pdf\\#src:@line@tex"
+        vim.g.vimtex_view_method = "general"
+        vim.g.vimtex_view_general_viewer = "okular"
+        vim.g.vimtex_view_general_options = "--unique file:@pdf\\#src:@line@tex"
+        vim.g.vimtex_view_use_temp_files = 0
+        vim.g.vimtex_view_enabled = 1
 
         -- Deactivate quickfix window
         vim.g["vimtex_quickfix_mode"] = 0
