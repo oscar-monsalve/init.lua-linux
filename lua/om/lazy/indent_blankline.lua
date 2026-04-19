93b7b48f859cbd7c9091a26ab5c0e925bf1debdf
Oscar Monsalve <oscarmonsalvec5@gmail.com>
2026-04-18 19:16:50 -0500

    added custom color to indent_blankline.lua

--- lua/om/lazy/indent_blankline.lua
+++ lua/om/lazy/indent_blankline.lua
@@ -1,10 +1,24 @@
-return{
+return {
     "lukas-reineke/indent-blankline.nvim",
     main = "ibl",
     ---@module "ibl"
     ---@type ibl.config
     opts = {
-        indent = { char = "▏" },
-        scope = { enabled = false},
+        indent = {
+            char = "▏",
+            highlight = "IblIndent",
+        },
+        scope = { enabled = false },
     },
+    config = function(_, opts)
+        local hooks = require("ibl.hooks")
+        -- create the highlight groups in the highlight setup hook, so they are reset
+        -- every time the colorscheme changes
+        hooks.register(hooks.type.HIGHLIGHT_SETUP, function()
+            -- Replace "#E5C07B" with your preferred contrast color
+            vim.api.nvim_set_hl(0, "IblIndent", { fg = "#636772" })
+        end)
+
+        require("ibl").setup(opts)
+    end,
 }
