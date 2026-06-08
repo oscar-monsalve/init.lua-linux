c17a9cd35852b9b4bc0b00526e574c48fb1ad516
Oscar Monsalve <oscarmonsalvec5@gmail.com>
2026-06-08 16:40:05 -0500

    simplified hover config in init.lua

--- lua/om/init.lua
+++ lua/om/init.lua
@@ -88,7 +88,8 @@ autocmd('LspAttach', {
     callback = function(e)
         local opts = { buffer = e.buf }
         vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
-        vim.keymap.set("n", "K", function() vim.lsp.buf.hover(vim.tbl_deep_extend("force", {}, { border = "rounded" })) end, opts)
+        -- vim.keymap.set("n", "K", function() vim.lsp.buf.hover(vim.tbl_deep_extend("force", {}, { border = "rounded" })) end, opts)
+        vim.keymap.set("n", "K", function() vim.lsp.buf.hover({ border = "rounded" }) end, opts)
         vim.keymap.set("i", "<C-h>", function() vim.lsp.buf.signature_help(vim.tbl_deep_extend("force", {}, { border = "rounded" })) end, opts)
         vim.keymap.set("n", "<leader>vws", function() vim.lsp.buf.workspace_symbol() end, opts)
         vim.keymap.set("n", "<leader>vd", function() vim.diagnostic.open_float() end, opts)
